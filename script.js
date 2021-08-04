fb3e40bf8b81380bfe38f48877bb4632038a076a
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-04 10:37:43 +0530

    Changed file name

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Selecting elements
+const score0El = document.getElementById("Score--0");
+const score1El = document.getElementById("Score--1");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
